0f88e2eba837d04f3f926425eded0a02297f73a0
appukappu <aparnareddy312@gmail.com>
2023-10-30 22:54:22 +0530

    ifelse

---
+++ Student2.java
@@ -0,0 +1,20 @@
+public class Student2 {
+    private String name;
+    private int rollno;
+    private int marks;
+
+    public Student2(String name,int rollno,int marks) {
+        this.name = name;
+        this.rollno = rollno;
+        this.marks = marks;
+    }
+
+
+
+        public int getmarks(){
+            return this.marks;
+        }
+
+
+}
+
---
+++ Test.java
@@ -0,0 +1,33 @@
+public class Test {
+
+    public Student2 getpassedstudent(Student2 s) {
+
+
+        if (s.getmarks() > 35) {
+            return s;
+        }
+     else {
+    return null;
+        }
+
+    }
+    public static void main(String[]args){
+
+        Student2 s1 = new Student2("chandu", 22, 50);
+
+        Student2 s2 = new Student2("sarath", 45, 32);
+        Test t = new Test();
+        Student2 ss = t.getpassedstudent(s1);
+        if(ss !=null){
+            System.out.println(ss.getmarks());
+        }
+
+        Student2 sss = t.getpassedstudent(s2);
+        if(sss != null){
+            System.out.println(sss.getmarks());
+        }
+
+
+
+    }
+}
